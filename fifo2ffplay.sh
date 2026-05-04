0c714a2342baeefd9b337978be9800b097266af2
Alex T <lexkie@proton.me>
2026-05-03 23:07:43 -0400

    Add new audio input for FFmpeg processing

--- fifo2ffplay.sh
+++ fifo2ffplay.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # 2. Run FFmpeg (run in background or separate termux session
 ffmpeg -hide_banner -loglevel error -f s16le -ar 44100 -ac 2 -i ~/mpd.fifo \
-       -i /storage/emulated/0/DolbyProLogic2.irs \
+       -i /storage/emulated/0/dearvr.wav \
        -filter_complex "[0:a][1:a]afir=dry=1:wet=1[out]" \
        -map "[out]" -f wav - | \
 ffplay -nodisp -af "bs2b=fcut=650:feed=95,dynaudnorm=g=5:p=0.9" -fflags nobuffer -flags low_delay -i - >/dev/null 2>&1 &
